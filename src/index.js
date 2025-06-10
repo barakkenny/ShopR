8b511d3088d98d634ce3b4de5b8924a185a6b4f4
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-06-10 18:40:17 +0100

    Added sequelize and category model

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const {PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = require('./config/server-config');
 const apiRoute = require('./routes/apiRoute');
 const bodyParser = require('body-parser');
+const db = require('./config/db_config')
 // const responseTime = require('response-time')
-const mysql = require('mysql2')
+const { PORT} = require('./config/server-config');
 
 const app = express();
 
-
+const Category = require('./models/category');
 // app.use(responseTime())
 
-const db = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
@@ -23,22 +17,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', apiRoute);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 
-    db.connect((err) => {
-        if (err) {
-            console.error('Error connecting to the database:', err);
-            throw err;
-        }
-        console.log('DB Connected');
+    // db.connect((err) => {
+    //     if (err) {
+    //         console.error('Error connecting to the database:', err);
+    //         throw err;
+    //     }
+    //     console.log('DB Connected');
 
-        db.query('Select * from products', (err, result)=>{
-            if(err) {
-                console.error('Error fetching products:', err);
-                throw err;
-            }
-            console.log(result[0]);
-        })
+    //     db.query('Select * from products', (err, result)=>{
+    //         if(err) {
+    //             console.error('Error fetching products:', err);
+    //             throw err;
+    //         }
+    //         console.log(result[0]);
+    //     })
+    // });
+
+    await db.sync()
+    console.log('DB connected'); 
+    const res = await Category.create({
+        name: 'Electronics',
+        description: 'Category for electronic products'
     });
+    console.log(res)
 }); 
